7eddb68a9315e3475be5404c0a91820b9fdcf81d
AlssyLop <alcibiades.lopezf@gmail.com>
2026-03-05 01:03:12 -0500

    Achievement: Improved registration page by implementing a multi-step form

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -6,24 +6,35 @@ import { ButtonComponent } from 'src/app/components/button/button.component';
 import { EmailInputComponent } from 'src/app/components/email-input/email-input.component';
 import { PasswordInputComponent } from 'src/app/components/password-input/password-input.component';
 
+import { BackButtonComponent } from 'src/app/components/back-button/back-button.component';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
   standalone: true,
-  imports: [IonicModule, ButtonComponent, CommonModule, FormsModule, ReactiveFormsModule, EmailInputComponent, PasswordInputComponent]
+  imports: [IonicModule, ButtonComponent, CommonModule, FormsModule, ReactiveFormsModule, EmailInputComponent, PasswordInputComponent, BackButtonComponent]
 })
 export class RegisterPage implements OnInit {
-  loginForm!: FormGroup
+  currentStep: number = 1;
+  registerForm!: FormGroup
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-    this.loginForm = this.fb.group({
+    this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       firstName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$')]],
       lastName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$')]],
-      password: ['', [Validators.required]]
+      password: ['', [Validators.required], Validators.minLength(8)]
     });
+  }
+
+  nextStep() {
+    if (this.currentStep < 3) this.currentStep++;
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) this.currentStep--;
   }
 
 }
